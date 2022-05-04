e8bdc9c056b73309b2cacd2064b9b42376405e72
Kotaro Mammaru <kotaro.112358@icloud.com>
2022-05-04 18:52:46 +0900

    はてな

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="timeline">
+        <h1>About Page</h1>
+        <Post />
+        <Post />
+    </div>
+    
+</template>
+
+<style>
+    .timeline{
+        background-color: rgba(189, 218, 218, 0.854);
+    }
+</style>
